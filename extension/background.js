bd6942993b49ec53d60b65947f1d45794081d8bb
Roman Nurik <roman@nurik.net>
2019-02-28 16:35:16 -0500

    Fix search results URL

--- extension/background.js
+++ extension/background.js
@@ -255,7 +255,7 @@ function onScriptsLoaded() {
     if (text.match(/^https?\:/)) {
       navigateToUrl(text);
     } else {
-      navigateToUrl('https://developer.android.com/index.html?q=' +
+      navigateToUrl('https://developer.android.com/s/results/?q=' +
           encodeURIComponent(text));
     }
   });
